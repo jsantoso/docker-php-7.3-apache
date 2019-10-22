6d47a6e96206171159ccb460452a90647659ac85
Jeffrey Santoso <jeffrey.k.santoso@gmail.com>
2019-10-22 21:47:39 +1100

    Do not create temporary build tag

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,8 +2,6 @@
 
 docker login
 
-docker build -t jsantoso_php-7.3-apache .
-
-docker tag jsantoso_php-7.3-apache jsantoso/php-7.3-apache:latest
+docker build -t jsantoso/php-7.3-apache:latest .
 
 docker push jsantoso/php-7.3-apache:latest
